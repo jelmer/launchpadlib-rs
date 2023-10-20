da31ad4e4c215c53e814ad90c9e8603cedc53dfd
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-10-20 20:02:50 +0100

    Use debversion::Version

--- src/lib.rs
+++ src/lib.rs
@@ -13,21 +13,27 @@
 //! println!("Service root: {:?}", service_root);
 //!
 //! let url = Url::parse("https://api.launchpad.net/1.0/ubuntu/+archive/primary").unwrap();
-#![allow(unused_mut)]
+
+use url::Url;
+use wadl::{Error, Resource};
+
 #[cfg(feature = "api-devel")]
 pub mod devel {
+    #![allow(unused_mut)]
     use super::*;
     include!(concat!(env!("OUT_DIR"), "/generated/devel.rs"));
 }
 
 #[cfg(feature = "api-beta")]
 pub mod beta {
+    #![allow(unused_mut)]
     use super::*;
     include!(concat!(env!("OUT_DIR"), "/generated/beta.rs"));
 }
 
 #[cfg(feature = "api-v1_0")]
 pub mod v1_0 {
+    #![allow(unused_mut)]
     use super::*;
 
     include!(concat!(env!("OUT_DIR"), "/generated/1_0.rs"));
@@ -65,51 +71,5 @@ pub mod v1_0 {
         } else {
             Err(Error::InvalidUrl)
         }
-    }
-}
-
-use url::Url;
-
-/// The root of the web service.
-pub trait Resource {
-    fn url(&self) -> Url;
-}
-
-#[derive(Debug)]
-pub enum Error {
-    InvalidUrl,
-    Reqwest(reqwest::Error),
-    Url(url::ParseError),
-    Json(serde_json::Error),
-}
-
-impl From<serde_json::Error> for Error {
-    fn from(err: serde_json::Error) -> Self {
-        Error::Json(err)
-    }
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            Error::InvalidUrl => write!(f, "Invalid URL"),
-            Error::Reqwest(err) => write!(f, "Reqwest error: {}", err),
-            Error::Url(err) => write!(f, "URL error: {}", err),
-            Error::Json(err) => write!(f, "JSON error: {}", err),
-        }
-    }
-}
-
-impl std::error::Error for Error {}
-
-impl From<reqwest::Error> for Error {
-    fn from(err: reqwest::Error) -> Self {
-        Error::Reqwest(err)
-    }
-}
-
-impl From<url::ParseError> for Error {
-    fn from(err: url::ParseError) -> Self {
-        Error::Url(err)
     }
 }
