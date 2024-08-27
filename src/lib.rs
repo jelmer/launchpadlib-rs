fcccf539c6bc123cf85ce57f31df86ef3cb4b0f0
Jelmer Vernooij <jelmer@jelmer.uk>
2024-08-27 02:02:05 +0000

    Move tests to separate module

--- src/lib.rs
+++ src/lib.rs
@@ -52,7 +52,10 @@ pub mod devel {
     /// let client = launchpadlib::Client::anonymous("just+testing");
     /// let root = launchpadlib::devel::service_root_for_host(&client, "api.staging.launchpad.net").unwrap();
     /// ```
-    pub fn service_root_for_host(client: &dyn wadl::Client, host: &str) -> std::result::Result<ServiceRootJson, Error> {
+    pub fn service_root_for_host(
+        client: &dyn wadl::Client,
+        host: &str,
+    ) -> std::result::Result<ServiceRootJson, Error> {
         let url = Url::parse(&format!("https://{}/devel/", host)).unwrap();
         ServiceRoot(url).get(client)
     }
@@ -84,7 +87,10 @@ pub mod beta {
     /// let client = launchpadlib::Client::anonymous("just+testing");
     /// let root = launchpadlib::beta::service_root_for_host(&client, "api.staging.launchpad.net").unwrap();
     /// ```
-    pub fn service_root_for_host(client: &dyn wadl::Client, host: &str) -> std::result::Result<ServiceRootJson, Error> {
+    pub fn service_root_for_host(
+        client: &dyn wadl::Client,
+        host: &str,
+    ) -> std::result::Result<ServiceRootJson, Error> {
         let url = Url::parse(&format!("https://{}/beta/", host)).unwrap();
         ServiceRoot(url).get(client)
     }
@@ -111,7 +117,9 @@ pub mod v1_0 {
         };
     }
 
-    pub fn get_service_root_by_url(url: &'_ Url) -> std::result::Result<&'static ServiceRoot, Error> {
+    pub fn get_service_root_by_url(
+        url: &'_ Url,
+    ) -> std::result::Result<&'static ServiceRoot, Error> {
         if url == ROOT.url() {
             Ok(&ROOT)
         } else {
@@ -131,7 +139,10 @@ pub mod v1_0 {
     /// let client = launchpadlib::Client::anonymous("just+testing");
     /// let root = launchpadlib::v1_0::service_root_for_host(&client, "api.staging.launchpad.net").unwrap();
     /// ```
-    pub fn service_root_for_host(client: &dyn wadl::Client, host: &str) -> std::result::Result<ServiceRootJson, Error> {
+    pub fn service_root_for_host(
+        client: &dyn wadl::Client,
+        host: &str,
+    ) -> std::result::Result<ServiceRootJson, Error> {
         let url = Url::parse(&format!("https://{}/1.0/", host)).unwrap();
         ServiceRoot(url).get(client)
     }
@@ -146,39 +157,44 @@ pub mod v1_0 {
         }
     }
 
-    #[test]
-    fn test_parse_person() {
-        let json = include_str!("../testdata/person.json");
-        let person: PersonFull = serde_json::from_str(json).unwrap();
-        assert_eq!(person.display_name, "Jelmer Vernooĳ");
-    }
+    #[cfg(test)]
+    mod tests {
+        use super::*;
 
-    #[test]
-    fn test_parse_team() {
-        let json = include_str!("../testdata/team.json");
-        let team: TeamFull = serde_json::from_str(json).unwrap();
-        assert_eq!(team.display_name, "awsome-core");
+        #[test]
+        fn test_parse_person() {
+            let json = include_str!("../testdata/person.json");
+            let person: PersonFull = serde_json::from_str(json).unwrap();
+            assert_eq!(person.display_name, "Jelmer Vernooĳ");
+        }
 
-        let json = include_str!("../testdata/team2.json");
-        let team: TeamFull = serde_json::from_str(json).unwrap();
-    }
+        #[test]
+        fn test_parse_team() {
+            let json = include_str!("../testdata/team.json");
+            let team: TeamFull = serde_json::from_str(json).unwrap();
+            assert_eq!(team.display_name, "awsome-core");
 
-    #[test]
-    fn test_parse_bug() {
-        let json = include_str!("../testdata/bug.json");
-        let bug: BugFull = serde_json::from_str(json).unwrap();
-        assert_eq!(bug.title, "Microsoft has a majority market share");
+            let json = include_str!("../testdata/team2.json");
+            let team: TeamFull = serde_json::from_str(json).unwrap();
+        }
 
-        let json = include_str!("../testdata/bug2.json");
-        let bug: BugFull = serde_json::from_str(json).unwrap();
-        assert_eq!(bug.name, None);
-        assert_eq!(bug.id, 2039729);
-    }
+        #[test]
+        fn test_parse_bug() {
+            let json = include_str!("../testdata/bug.json");
+            let bug: BugFull = serde_json::from_str(json).unwrap();
+            assert_eq!(bug.title, "Microsoft has a majority market share");
 
-    #[test]
-    fn test_parse_bug_tasks() {
-        let json = include_str!("../testdata/bug_tasks.json");
-        let bug_tasks: BugTaskPage= serde_json::from_str(json).unwrap();
+            let json = include_str!("../testdata/bug2.json");
+            let bug: BugFull = serde_json::from_str(json).unwrap();
+            assert_eq!(bug.name, None);
+            assert_eq!(bug.id, 2039729);
+        }
+
+        #[test]
+        fn test_parse_bug_tasks() {
+            let json = include_str!("../testdata/bug_tasks.json");
+            let bug_tasks: BugTaskPage = serde_json::from_str(json).unwrap();
+        }
     }
 
     impl Bugs {
@@ -190,14 +206,22 @@ pub mod v1_0 {
         /// let root = launchpadlib::v1_0::service_root(&client).unwrap();
         /// let bug = root.bugs().unwrap().get_by_id(&client, 1).unwrap();
         /// ```
-        pub fn get_by_id(&self, client: &dyn wadl::Client, id: u32) -> std::result::Result<BugFull, Error> {
+        pub fn get_by_id(
+            &self,
+            client: &dyn wadl::Client,
+            id: u32,
+        ) -> std::result::Result<BugFull, Error> {
             let url = self.url().join(format!("bugs/{}", id).as_str()).unwrap();
             Bug(url).get(client)
         }
     }
 
     impl Projects {
-        pub fn get_by_name(&self, client: &dyn wadl::Client, name: &str) -> std::result::Result<ProjectFull, Error> {
+        pub fn get_by_name(
+            &self,
+            client: &dyn wadl::Client,
+            name: &str,
+        ) -> std::result::Result<ProjectFull, Error> {
             let url = self.url().join(name).unwrap();
             Project(url).get(client)
         }
@@ -232,12 +256,20 @@ pub mod v1_0 {
 
     impl People {
         /// Get a person or team by name
-        pub fn get_by_name(&self, client: &dyn wadl::Client, name: &str) -> std::result::Result<PersonOrTeam, Error> {
+        pub fn get_by_name(
+            &self,
+            client: &dyn wadl::Client,
+            name: &str,
+        ) -> std::result::Result<PersonOrTeam, Error> {
             let url = self.url().join(&format!("~{}", name)).unwrap();
 
             let wadl = wadl::get_wadl_resource_by_href(client, &url)?;
 
-            let types = wadl.r#type.iter().filter_map(|t| t.id()).collect::<Vec<_>>();
+            let types = wadl
+                .r#type
+                .iter()
+                .filter_map(|t| t.id())
+                .collect::<Vec<_>>();
 
             if types.contains(&"person") {
                 Ok(PersonOrTeam::Person(Person(url)))
