08e85dd97904f3ae80a08f12453139771892eb84
Jelmer Vernooij <jelmer@jelmer.uk>
2024-08-27 02:02:05 +0000

    Move tests to separate module

--- src/lib.rs
+++ src/lib.rs
@@ -166,39 +166,44 @@ pub mod v1_0 {
         }
     }
 
-    #[test]
-    fn test_parse_person() {
-        let json = include_str!("../testdata/person.json");
-        let person: PersonFull = serde_json::from_str(json).unwrap();
-        assert_eq!(person.display_name, "Jelmer Vernooĳ");
-    }
+    #[cfg(test)]
+    mod tests {
+        use super::*;
 
-    #[test]
-    fn test_parse_team() {
-        let json = include_str!("../testdata/team.json");
-        let team: TeamFull = serde_json::from_str(json).unwrap();
-        assert_eq!(team.display_name, "awsome-core");
+        #[test]
+        fn test_parse_person() {
+            let json = include_str!("../testdata/person.json");
+            let person: PersonFull = serde_json::from_str(json).unwrap();
+            assert_eq!(person.display_name, "Jelmer Vernooĳ");
+        }
 
-        let json = include_str!("../testdata/team2.json");
-        let _team: TeamFull = serde_json::from_str(json).unwrap();
-    }
+        #[test]
+        fn test_parse_team() {
+            let json = include_str!("../testdata/team.json");
+            let team: TeamFull = serde_json::from_str(json).unwrap();
+            assert_eq!(team.display_name, "awsome-core");
 
-    #[test]
-    fn test_parse_bug() {
-        let json = include_str!("../testdata/bug.json");
-        let bug: BugFull = serde_json::from_str(json).unwrap();
-        assert_eq!(bug.title, "Microsoft has a majority market share");
+            let json = include_str!("../testdata/team2.json");
+            let _team: TeamFull = serde_json::from_str(json).unwrap();
+        }
 
-        let json = include_str!("../testdata/bug2.json");
-        let bug: BugFull = serde_json::from_str(json).unwrap();
-        assert_eq!(bug.name, None);
-        assert_eq!(bug.id, 2039729);
-    }
+        #[test]
+        fn test_parse_bug() {
+            let json = include_str!("../testdata/bug.json");
+            let bug: BugFull = serde_json::from_str(json).unwrap();
+            assert_eq!(bug.title, "Microsoft has a majority market share");
 
-    #[test]
-    fn test_parse_bug_tasks() {
-        let json = include_str!("../testdata/bug_tasks.json");
-        let _bug_tasks: BugTaskPage = serde_json::from_str(json).unwrap();
+            let json = include_str!("../testdata/bug2.json");
+            let bug: BugFull = serde_json::from_str(json).unwrap();
+            assert_eq!(bug.name, None);
+            assert_eq!(bug.id, 2039729);
+        }
+
+        #[test]
+        fn test_parse_bug_tasks() {
+            let json = include_str!("../testdata/bug_tasks.json");
+            let _bug_tasks: BugTaskPage = serde_json::from_str(json).unwrap();
+        }
     }
 
     impl Bugs {
