d580baa8c4bcbe4cebcdfb0252fe42bcd60aafb3
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-10-19 19:42:56 +0000

    When enabling keyring, also enable some backends

---
+++ examples/keyring.rs
@@ -0,0 +1,7 @@
+fn main() {
+    env_logger::init();
+    let lp = launchpadlib::Client::authenticated(None, "example-keyring").unwrap();
+    let root = launchpadlib::v1_0::service_root(&lp).unwrap();
+    let person = root.me().unwrap().get(&lp).unwrap();
+    println!("{:?}", person);
+}
